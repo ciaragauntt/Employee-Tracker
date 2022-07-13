cea66108ab8ce1f8398d19083fa1b37570968a2b
Ciara Gauntt <ci.gauntt@gmail.com>
2022-07-13 17:12:10 -0600

    first commit: added schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE employee_role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(5,2),
+    department_id INT
+)
+
+CREATE TABLE employee_info (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+)
